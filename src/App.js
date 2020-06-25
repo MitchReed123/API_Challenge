e15f6e304447c7fb2a4717ac8c17a97a015cee23
Mitchell Reed <mitchellreed@Mitchells-MacBook-Pro.local>
2020-06-25 12:02:22 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import FetchAll from "./components/fetch/fetchAll";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class Display extends React.Component {
+  // get coords here, pass them into each file
+  render() {
+    return (
+      <div>
+        <FetchAll />
+      </div>
+    );
+  }
 }
 
-export default App;
+export default Display;
---
+++ src/components/fetch/displayFetch/displayFetch.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+const DisplayFetch = (props) => {
+  return (
+    <div>
+      <img src={props.url} alt="test" />
+    </div>
+  );
+};
+
+export default DisplayFetch;
